c108f06bf703f6543391ac4200d0fd05c2ac0612
Sourav Mazumder <mazumdersourav2000@gmail.com>
2023-07-25 13:47:06 +0530

    CORS support

--- MyntraClone/src/main/java/com/myntra/MyntraCloneApplication.java
+++ MyntraClone/src/main/java/com/myntra/MyntraCloneApplication.java
@@ -3,17 +3,25 @@ package com.myntra;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @PropertySource(value = { "classpath:messages.properties" })
 @EnableWebMvc
 @EnableSwagger2
-public class MyntraCloneApplication {
+public class MyntraCloneApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyntraCloneApplication.class, args);
+	}
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET","POST","DELETE","PUT");
 	}
 
 }
